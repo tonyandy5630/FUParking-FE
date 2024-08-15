84aa63cfd57db87c47bfdd46e28a9e4122382c58
Bùi Thanh Tú <61198577+tonyandy5630@users.noreply.github.com>
2024-08-16 03:32:09 +0700

    <style> style layout components (#31)

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,77 +1,63 @@
-'use client';
+"use client";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import { useMemo, useState } from "react";
 
 interface HeaderProps {
-    isOpen: boolean;
-    setIsOpen: (value: boolean) => void;
-    height: number;
+  isOpen: boolean;
+  setIsOpen: (value: boolean) => void;
+  height: number;
 }
 
 export default function Header({ isOpen, setIsOpen, height }: HeaderProps) {
-    const router = usePathname();
-    let title;
+  const pathname = usePathname();
 
-    const segments = router.split('/');
-
+  const title = useMemo(() => {
+    const segments = pathname.split("/");
     switch (segments[2]) {
-        case "customer":
-            title = "Customer";
-            break;
-        case "customer":
-            title = "Customer";
-            break;
-        case "transaction":
-            title = "Transaction";
-            break;
-        case "card":
-            title = "Card";
-            break;
-        case "vehicle":
-            title = "Vehicle";
-            break;
-        case "parking-area":
-            title = "Parking Area";
-            break;
-        case "feedback":
-            title = "Feedback";
-            break;
-        case "gate":
-            title = "Gate";
-            break;
-        case "price":
-            title = "Price";
-            break;
-        case "package":
-            title = "Package";
-            break;
-        case "user":
-            title = "User";
-            break;
-        case "vehicle-type":
-            title = "Vehicle Type";
-            break;
-        default:
-            title = "Dashboard";
-            break;
+      case "customer":
+        return "Customer";
+      case "customer":
+        return "Customer";
+      case "transaction":
+        return "Transaction";
+      case "card":
+        return "Card";
+      case "vehicle":
+        return "Vehicle";
+      case "parking-area":
+        return "Parking Area";
+      case "feedback":
+        return "Feedback";
+      case "gate":
+        return "Gate";
+      case "price":
+        return "Price";
+      case "package":
+        return "Package";
+      case "user":
+        return "User";
+      case "vehicle-type":
+        return "Vehicle Type";
+      default:
+        return "Dashboard";
     }
+  }, [pathname]);
 
-    return (
-        <header
-            className="flex flex-row items-center pl-5 gap-5 shadow-md"
-            style={{ backgroundColor: '#111827', height: `${height}px` }}
-        >
-            <IconButton onClick={() => setIsOpen(!isOpen)} color="warning">
-                <MenuIcon />
-            </IconButton>
-            <div className="flex flex-row justify-center items-center">
-                <Image src="/bai_logo.svg" alt="Bai Logo" width={60} height={60} />
-                <p
-                    className="text-2xl font-bold text-white ml-4"
-                >{title}</p>
-            </div>
-        </header>
-    )
+  return (
+    <header
+      className='flex flex-row items-center pl-5 gap-5 shadow-md'
+      style={{ backgroundColor: "#111827", height: `${height}px` }}
+    >
+      <IconButton onClick={() => setIsOpen(!isOpen)} color='warning'>
+        <MenuIcon />
+      </IconButton>
+      <div className='flex flex-row justify-center items-center'>
+        <Image src='/bai_logo.svg' alt='Bai Logo' width={60} height={60} />
+        <p className='text-2xl font-bold text-white ml-4'>{title}</p>
+      </div>
+    </header>
+  );
 }
