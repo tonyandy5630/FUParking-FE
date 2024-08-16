d4a26e10288db448533daa268345851f2cc66375
Bùi Thanh Tú <61198577+tonyandy5630@users.noreply.github.com>
2024-08-16 15:00:51 +0700

    <mod> minor change to price table and details page (#32)

--- api/url/price-item.url.ts
+++ api/url/price-item.url.ts
@@ -1,7 +1,13 @@
+import { PaginationType } from "@/types/pagination.type";
 import baseURL from ".";
 
-export const GET_ALL_PRICE_ITEM_BY_TABLE_API_URL = (id: string) =>
-  `${baseURL}/price/${id}/items`;
+export const GET_ALL_PRICE_ITEM_BY_TABLE_API_URL = (
+  id: string,
+  pagination: PaginationType
+) =>
+  `${baseURL}/price/${id}/items?PageSize=${pagination.pageSize}&PageIndex=${
+    pagination.pageIndex + 1
+  }`;
 
 export const CREATE_PRICE_ITEMS_API_URL = `${baseURL}/price/vehicle/item`;
 
