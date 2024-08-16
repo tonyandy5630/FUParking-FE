152e016a73559df047f9efee621b639fb321ee64
tonyandy5630 <tonyandy456@gmail.com>
2024-08-16 23:35:38 +0700

    <feat> add confirm box for customer

---
+++ components/ActionButton/index.tsx
@@ -0,0 +1,35 @@
+import { Button } from "@mui/material";
+import React from "react";
+
+interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant: "primary" | "danger";
+}
+
+const ActionButton = React.forwardRef<HTMLButtonElement, Props>(
+  ({ variant, onClick, disabled, children, ...props }, ref) => {
+    return (
+      <Button
+        sx={{
+          backgroundColor: `${variant === "primary" ? "#3b82f6" : "#ef4444"}`,
+          color: "white",
+          width: "80px",
+          "&:disabled": {
+            backgroundColor: "grey",
+            color: "white",
+          },
+          "&:hover": {
+            backgroundColor: `${variant === "primary" ? "#2563eb" : "#dc2626"}`,
+          },
+        }}
+        onClick={onClick}
+        disabled={disabled}
+      >
+        {children}
+      </Button>
+    );
+  }
+);
+
+ActionButton.displayName = "ActionButton";
+
+export default ActionButton;
