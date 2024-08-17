565983443a5482013c5750bcfa83bf80b1053728
tonyandy5630 <tonyandy456@gmail.com>
2024-08-17 18:52:38 +0700

    <mod> refactor package table

---
+++ components/Page/Package/table-headers.ts
@@ -0,0 +1,10 @@
+export const PackageTableHeaders = [
+  "No.",
+  "Name",
+  "Coin Amount",
+  "Extra Coin",
+  "Exp Package",
+  "Price",
+  "Status",
+  "Created Date",
+];
--- hook/useSearchDebouce.ts
+++ hook/useSearchDebouce.ts
@@ -1,14 +1,17 @@
 import { DEBOUNCE_DELAY } from "@/constant/debounce";
 import { useState } from "react";
-import { useDebounce } from "use-debounce";
+import { useDebounce, useDebouncedCallback } from "use-debounce";
 
 export default function useSearchDebounce(callback: () => void) {
   const [searchText, setSearchText] = useState("");
   const [debounceSearchText] = useDebounce(searchText, DEBOUNCE_DELAY);
+  const debounceCallback = useDebouncedCallback(() => {
+    callback();
+  }, DEBOUNCE_DELAY);
 
   const handleSearchTextChange = (value: string) => {
     setSearchText(value);
-    callback();
+    // debounceCallback();
   };
 
   return {
