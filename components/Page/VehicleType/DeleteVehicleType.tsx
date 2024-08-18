30e7493898671bf8b4cacadd5b8e6029264426b5
tonyandy5630 <tonyandy456@gmail.com>
2024-08-19 04:07:34 +0700

    <mod> add confirm dialog to delete vehicle type

--- components/Page/VehicleType/DeleteVehicleType.tsx
+++ components/Page/VehicleType/DeleteVehicleType.tsx
@@ -1,61 +1,55 @@
 import { deleteVehicleTypeAPI } from "@/api/vehicleType";
+import MyDeleteButton from "@/components/DeleteButton";
 import { ListVehicleTypeResponse } from "@/types/vehicleType.type";
 import { Button } from "@mui/material";
-import { QueryObserverResult, RefetchOptions, useMutation } from "@tanstack/react-query";
+import {
+  QueryObserverResult,
+  RefetchOptions,
+  useMutation,
+} from "@tanstack/react-query";
 import { AxiosResponse } from "axios";
 import { toast } from "react-toastify";
 
-export default function DeleteVehicleType({ id, refetch, setIsPending, disable }: { id: string, refetch: (options?: RefetchOptions) => Promise<QueryObserverResult<AxiosResponse<ListVehicleTypeResponse, any>, Error>>, setIsPending: (isPending: boolean) => void, disable: boolean }) {
+export default function DeleteVehicleType({
+  id,
+  refetch,
+  setIsPending,
+  disable,
+}: {
+  id: string;
+  refetch: (
+    options?: RefetchOptions
+  ) => Promise<
+    QueryObserverResult<AxiosResponse<ListVehicleTypeResponse, any>, Error>
+  >;
+  setIsPending: (isPending: boolean) => void;
+  disable: boolean;
+}) {
+  const deleteVehicleTypeMutation = useMutation({
+    mutationKey: ["/types"],
+    mutationFn: (id: string) => deleteVehicleTypeAPI(id),
+    onMutate: () => {
+      setIsPending(true);
+    },
+  });
 
-    const deleteVehicleTypeMutation = useMutation({
-        mutationKey: ["/types"],
-        mutationFn: (id: string) => deleteVehicleTypeAPI(id),
-        onMutate: () => {
-            setIsPending(true);
-        }
-    });
-
-    const onDelete = async () => {
-        try {
-            await deleteVehicleTypeMutation.mutateAsync(id, {
-                onSuccess: (data) => {
-                    toast.success('Vehicle type deleted successfully');
-                    refetch();
-                    setIsPending(false);
-                },
-                onError: () => {
-                    toast.error('Failed to delete vehicle type');
-                    setIsPending(false);
-                    refetch();
-                }
-            });
-        } catch (error) {
-            toast.error('Failed to delete vehicle type');
-            setIsPending(false);
-            refetch();
-        }
+  const onDelete = async () => {
+    try {
+      await deleteVehicleTypeMutation.mutateAsync(id, {
+        onSuccess: (data) => {
+          toast.success("Vehicle type deleted successfully");
+          refetch();
+          setIsPending(false);
+        },
+      });
+    } catch (error) {
+      setIsPending(false);
     }
+  };
 
-    return (
-        <>
-            <Button
-                sx={{
-                    backgroundColor: '#ef4444',
-                    color: 'white',
-                    width: '80px',
-                    '&:disabled': {
-                        backgroundColor: 'grey',
-                        color: 'white',
-                    },
-                    '&:hover': {
-                        backgroundColor: '#dc2626',
-                    }
-                }}
-                disabled={disable}
-                onClick={onDelete}
-            >
-                Delete
-            </Button>
-        </>
-    );
+  return (
+    <>
+      <MyDeleteButton onDelete={onDelete}>DELETE</MyDeleteButton>
+    </>
+  );
 }
